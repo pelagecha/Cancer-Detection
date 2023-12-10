d9c45d06d0a733c9b45d74886d4b5ae2b2a4c33a
amency <nikitapelagecha@gmail.com>
2023-12-10 19:50:36 +0000

    fourth commit

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+import os
+import torchvision
 import torch
 import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
+
+data_dir = "cifar10"
+classes = os.listdir(data_dir + "/train")
+
+
