93e75a15b725109137a970f928da657aa5a755e7
Jakub Slapek <Jakub.Slapek@warwick.ac.uk>
2023-12-23 13:40:01 +0000

    pytorch template to improve main code later

---
+++ CNN.py
@@ -0,0 +1,191 @@
+import os
+
+import numpy as np
+#import cv2
+
+from PIL import Image
+#from IPython.display port display
+
+import torch
+import torch.nn as nn
+
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
+from torchvision import datasets, transforms, models
+from torch.optim.lr_scheduler import StepLR
+from tqdm import tqdm
+
+image_transform = transforms.Compose([
+    transforms.Resize(),
+    transforms.CenterCrop(),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.485, 0.456, 0.406])
+])
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        # Input Block
+        self.convblock1 = nn.Sequential(
+        nn.Conv2d(in_channels=3, out_channels=8, kernel_size=(3, 3), padding=0, bias=False),
+        nn.ReLU(),
+        #nn.BatchNorm2d(4)
+        )
+        self.pool11 = nn.MaxPool2d(2, 2)
+
+        # CONVOLUTION BLOCK
+        self.convblock2 = nn.Sequential(
+            nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(3, 3), padding=0, bias=False),
+            nn.ReLU(),
+            #nn.BatchNorm2d(16)
+        )
+
+        # TRANSITION BLOCK
+        self.pool22 = nn.MaxPool2d(2, 2)
+
+        self.convblock3 = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=10, kernel_size=(1, 1), padding=0, bias=False),
+            #nn.BatchNorm2d(10),
+            nn.ReLU()
+        )
+        self.pool33 = nn.MaxPool2d(2, 2)
+
+        # CONVOLUTION BLOCK
+        self.convblock4 = nn.Sequential(
+            nn.Conv2d(in_channels=10, out_channels=10, kernel_size=(3, 3), padding=0, bias=False),
+            nn.ReLU(),
+            #nn.BatchNorm2d(10)
+        )
+
+        self.convblock5 = nn.Sequential(
+            nn.Conv2d(in_channels=10, out_channels=32, kernel_size=(1, 1), padding=0, bias=False),
+            #nn.BatchNorm2d(32),
+            nn.ReLU(),
+        )
+
+        self.convblock6 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=10, kernel_size=(1, 1), padding=0, bias=False),
+            nn.ReLU(),
+            #nn.BatchNorm2d(10),
+        )
+
+        self.convblock7 = nn.Sequential(
+            nn.Conv2d(in_channels=10, out_channels=10, kernel_size=(3, 3), padding=0, bias=False),
+            nn.ReLU(),
+            #nn.BatchNorm2d(10)
+        )
+
+        self.convblock8 = nn.Sequential(
+            nn.Conv2d(in_channels=10, out_channels=32, kernel_size=(1, 1), padding=0, bias=False),
+            #nn.BatchNorm2d(32),
+            nn.ReLU()
+        )
+
+        self.convblock9 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=10, kernel_size=(1, 1), padding=0, bias=False),
+            nn.ReLU(),
+            #nn.BatchNorm2d(10),
+        )
+
+        self.convblock10 = nn.Sequential(
+            nn.Conv2d(in_channels=10, out_channels=14, kernel_size=(3, 3), padding=0, bias=False),
+            nn.ReLU(),
+            #nn.BatchNorm2d(14),
+        )
+
+        self.convblock11 = nn.Sequential(
+            nn.Conv2d(in_channels=14, out_channels=16, kernel_size=(3, 3), padding=0, bias=False),
+            nn.ReLU(),
+            #nn.BatchNorm2d(16),
+        )
+
+        # OUTPUT BLOCK
+        self.gap = nn.Sequential(
+            nn.AvgPool2d(kernel_size=4)
+        )
+
+        self.convblockout = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=2, kernel_size=(4, 4), padding=0, bias=False),
+        )
+
+    def forward(self, x):
+        x = self.convblock1(x)
+        x = self.pool11(x)
+        x = self.convblock2(x)
+        x = self.pool22(x)
+        x = self.convblock3(x)
+        x = self.pool33(x)
+        x = self.convblock4(x)
+        x = self.convblock5(x)
+        x = self.convblock6(x)
+        x = self.convblock7(x)
+        x = self.convblock8(x)
+        x = self.convblock9(x)
+        x = self.convblock10(x)
+        x = self.convblock11(x)
+        x = self.gap(x)
+        x = self.convblockout(x)
+        x = x.view(-1, 2)
+        return F.log_softmax(x, dim=-1)
+
+train_losses = []
+test_losses = []
+train_acc = []
+test_acc = []
+def train(model, device, train_loader, optimizer):
+    model.train()
+    pbar = tqdm(train_loader)
+    correct = 0
+    processed = 0
+
+    for batch_idx, (data, target) in enumerate(pbar):
+        # get data
+        data, target = data.to(device), target.to(device)
+        
+        # reset gradient
+        optimizer.zero_grad()
+        
+        # make prediction
+        pred = model(data)
+
+        # Calculating loss given the prediction
+        loss = F.nll_loss(pred, target)
+        train_losses.append(loss)
+
+        # Backprop
+        loss.backward()
+        optimizer.step()
+        # get indices of max values
+        pred_indices = pred.argmax(dim=1, keepdim=True)
+        correct += pred_indices.eq(target.view_as(pred_indices)).sum().item()
+        processed += len(data)
+        pbar.set_description(desc= f'Loss={loss.item()} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
+        train_acc.append(100*correct/processed)
+
+def test(model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target,
+            reduction='sum').item()
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    test_losses.append(test_loss)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{}({:.2f}%)\n'.format(test_loss, correct, len(test_loader.dataset), 100. * correct / len(test_loader.dataset)))
+    test_acc.append(100. * correct / len(test_loader.dataset))
+
+model = Net().todevice(device)
+optimizer = torch.optimizer = torch.optim.AdamW(model.parameters(), lr=1e-2)
+scheduler = StepLR(optimizer, step_size=6, gamma=0.5)
+
+epochs = 99999
+for epoch in range(epochs):
+    train(model, device, train_loader, optimizer)
+    scheduler.step()
